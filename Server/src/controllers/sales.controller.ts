5c0012068e83e85f4889d007b63f6b8838d04aef
tzSolo <t0504114734@gmail.com>
2026-05-13 02:46:11 +0300

    Change wrong params to query in sales.controller.

--- Server/src/controllers/sales.controller.ts
+++ Server/src/controllers/sales.controller.ts
@@ -3,7 +3,7 @@ import { getSalesDataByFrequency } from "../services/sales.service.js";
 
 export const getSales = async (req: Request, res: Response) => {
     try {
-        const range = req.params.about;
+        const range = req.query.about;
         if (range !== "day" && range !== "week" && range !== "month") {
             throw new Error("Range is not valid.");
         }
