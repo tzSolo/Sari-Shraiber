afa612927e16efbcff041b21ca6f0e50fe943fc2
tzSolo <t0504114734@gmail.com>
2026-05-13 03:02:11 +0300

    Cast range explicit in sales.controller.

--- Server/src/controllers/sales.controller.ts
+++ Server/src/controllers/sales.controller.ts
@@ -3,12 +3,12 @@ import { getSalesDataByFrequency } from "../services/sales.service.js";
 
 export const getSales = async (req: Request, res: Response) => {
     try {
-        const range = req.query.about;
-        if (range !== "day" && range !== "week" && range !== "month") {
+        const range = req.query.about as string;
+        if (!["day", "week", "month"].includes(range)) {
             throw new Error("Range is not valid.");
         }
 
-        const sales = await getSalesDataByFrequency(range);
+        const sales = await getSalesDataByFrequency(range as "day" | "week" | "month");
         return res.json(sales);
     }
     catch (e: any) {
