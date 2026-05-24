9fc070f4ab3632b542ad56b1812fa582adff0f5f
tzSolo <t0504114734@gmail.com>
2026-05-24 14:22:11 +0300

    Change name from about to range.

--- Server/src/controllers/sales.controller.ts
+++ Server/src/controllers/sales.controller.ts
@@ -3,7 +3,7 @@ import { getSalesDataByFrequency } from "../services/sales.service.js";
 
 export const getSales = async (req: Request, res: Response) => {
     try {
-        const range = req.query.about as string;
+        const range = req.query.range as string;
         if (!["day", "week", "month"].includes(range)) {
             throw new Error("Range is not valid.");
         }
